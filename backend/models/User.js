ff2584c7e7a0c0ffff521f4bd1b6e14f9c9e0b27
Navath-Lokesh <lokeshnavath@gmail.com>
2026-04-05 19:31:07 +0530

    reminder email error cleared

--- backend/models/User.js
+++ backend/models/User.js
@@ -30,10 +30,10 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false
     },
-    // lastReminderDate:{
-    //     type: Date,
-    //     default: null
-    // },
+    lastReminderDate:{
+        type: Date,
+        default: null
+    },
 
     emailVerificationToken: {
         type: String
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,33 +1,59 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+// const nodemailer = require("nodemailer");
+// require("dotenv").config();
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // App Password
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS, // App Password
+//   },
+// });
+
+// const sendEmail = async (to, subject, html) => {
+//   try {
+//     console.log("📩 Sending email to:", to);
+
+//     const info = await transporter.sendMail({
+//       from: `Trackiva <${process.env.EMAIL_USER}>`,
+//       // from: "Trackiva <onboarding@resend.dev>",
+//       to: to,
+//       subject: subject,
+//       html: html,
+//     });
+
+//     console.log("✅ Email sent:", info.response);
+
+//     return info;
+
+//   } catch (error) {
+//     console.error("❌ Email error:", error);
+//     return null;
+//   }
+// };
+
+// module.exports = sendEmail;
+
+
+
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
   try {
     console.log("📩 Sending email to:", to);
 
-    const info = await transporter.sendMail({
-      from: `Trackiva <${process.env.EMAIL_USER}>`,
-      // from: "Trackiva <onboarding@resend.dev>",
-      to: to,
-      subject: subject,
-      html: html,
+    await resend.emails.send({
+      from: "Trackiva <onboarding@resend.dev>", // ✅ works always
+      to,
+      subject,
+      html,
     });
 
-    console.log("✅ Email sent:", info.response);
-
-    return info;
+    console.log("✅ Email sent successfully");
 
   } catch (error) {
     console.error("❌ Email error:", error);
-    return null;
   }
 };
 
