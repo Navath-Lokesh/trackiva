72cd68dd98c5ba8e79076465f42134dd0ff18724
Navath-Lokesh <lokeshnavath@gmail.com>
2026-05-01 13:27:20 +0530

    error

--- frontend/src/pages/Habits.jsx
+++ frontend/src/pages/Habits.jsx
@@ -117,7 +117,7 @@ export default function Habits() {
 
           <div className="flex items-center gap-2">
             <h1 className="bg-green-200 rounded text-black px-2 text-sm">
-              April
+              May
             </h1>
 
             <div className="text-xs sm:text-sm text-gray-400">
